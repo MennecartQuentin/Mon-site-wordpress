efc1b43619ca9507bb3722b48f84fa4851311cc3
MennecartQuentin <mennecartquentin16@gmail.com>
2022-03-20 11:26:59 +0100

    4e commit

--- wp-admin/customize.php
+++ wp-admin/customize.php
@@ -2,13 +2,13 @@
 <html>
 	<head>
 		<title>Redirecting...</title>
-		<meta http-equiv="refresh" content="0;url=http://https://mennecartquentin.github.io/Mon-site-wordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fmon-site-vitrine%2Fwp-admin%2Fcustomize.php&reauth=1">
+		<meta http-equiv="refresh" content="0;url=https://mennecartquentin.github.io/Mon-site-wordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fmon-site-vitrine%2Fwp-admin%2Fcustomize.php&reauth=1">
 	</head>
 	<body>
 		<script type="text/javascript">
-			window.location = "http://https://mennecartquentin.github.io/Mon-site-wordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fmon-site-vitrine%2Fwp-admin%2Fcustomize.php&reauth=1";
+			window.location = "https://mennecartquentin.github.io/Mon-site-wordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fmon-site-vitrine%2Fwp-admin%2Fcustomize.php&reauth=1";
 		</script>
 
-		<p>You are being redirected to <a href="http://https://mennecartquentin.github.io/Mon-site-wordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fmon-site-vitrine%2Fwp-admin%2Fcustomize.php&reauth=1">http://https://mennecartquentin.github.io/Mon-site-wordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fmon-site-vitrine%2Fwp-admin%2Fcustomize.php&reauth=1</a></p>
+		<p>You are being redirected to <a href="https://mennecartquentin.github.io/Mon-site-wordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fmon-site-vitrine%2Fwp-admin%2Fcustomize.php&reauth=1">https://mennecartquentin.github.io/Mon-site-wordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fmon-site-vitrine%2Fwp-admin%2Fcustomize.php&reauth=1</a></p>
 	</body>
 </html>
