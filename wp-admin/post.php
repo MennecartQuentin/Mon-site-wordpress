ec9e48da766ecb78d55d5a8df026a7eb7641245e
MennecartQuentin <mennecartquentin16@gmail.com>
2022-03-24 17:34:11 +0100

    commit reset

--- wp-admin/post.php
+++ wp-admin/post.php
@@ -2,13 +2,13 @@
 <html>
 	<head>
 		<title>Redirecting...</title>
-		<meta http-equiv="refresh" content="0;url=https://mennecartquentin.github.io/Mon-site-wordpress/404/">
+		<meta http-equiv="refresh" content="0;url=https://github.com/MennecartQuentin/monSiteWordpress/404/">
 	</head>
 	<body>
 		<script type="text/javascript">
-			window.location = "https://mennecartquentin.github.io/Mon-site-wordpress/404/";
+			window.location = "https://github.com/MennecartQuentin/monSiteWordpress/404/";
 		</script>
 
-		<p>You are being redirected to <a href="https://mennecartquentin.github.io/Mon-site-wordpress/404/">https://mennecartquentin.github.io/Mon-site-wordpress/404/</a></p>
+		<p>You are being redirected to <a href="https://github.com/MennecartQuentin/monSiteWordpress/404/">https://github.com/MennecartQuentin/monSiteWordpress/404/</a></p>
 	</body>
 </html>
