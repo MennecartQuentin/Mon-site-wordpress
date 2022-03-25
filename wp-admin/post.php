5a18845a62940503485e9b1c0b61445cc58f443f
MennecartQuentin <mennecartquentin16@gmail.com>
2022-03-25 10:28:50 +0100

    ajout des pages suite erreur 404

--- wp-admin/post.php
+++ wp-admin/post.php
@@ -2,13 +2,13 @@
 <html>
 	<head>
 		<title>Redirecting...</title>
-		<meta http-equiv="refresh" content="0;url=https://mennecartquentin.github.io/Mon-site-wordpress/404/">
+		<meta http-equiv="refresh" content="0;url=/404/">
 	</head>
 	<body>
 		<script type="text/javascript">
-			window.location = "https://mennecartquentin.github.io/Mon-site-wordpress/404/";
+			window.location = "/404/";
 		</script>
 
-		<p>You are being redirected to <a href="https://mennecartquentin.github.io/Mon-site-wordpress/404/">https://mennecartquentin.github.io/Mon-site-wordpress/404/</a></p>
+		<p>You are being redirected to <a href="http://localhost/404/">/404/</a></p>
 	</body>
 </html>
